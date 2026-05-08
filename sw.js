cce934472dbb996ec75a2a6d3742880b044d0378
ChauHungCheungCyrus999 <chauhungcheungcyrus@gmail.com>
2026-05-08 10:25:15 +0800

    Site updated: 2026-05-08 10:25:14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1778206674687";const preCache = ["/PersonalBlog/images/taichi.png","/PersonalBlog/images/banner1.webp","/PersonalBlog/css/loader.css","/PersonalBlog/css/style.css","/PersonalBlog/js/script.js"];const cacheDomain = [
+const VERSION = "1778207111575";const preCache = ["/PersonalBlog/images/taichi.png","/PersonalBlog/images/banner1.webp","/PersonalBlog/css/loader.css","/PersonalBlog/css/style.css","/PersonalBlog/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
